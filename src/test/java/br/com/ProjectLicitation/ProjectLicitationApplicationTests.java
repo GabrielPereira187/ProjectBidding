114bf4ce43d23fb1f9d4c8460e31437decf3e649
GabrielPereira187 <gdebarrospereira@gmail.com>
2021-10-07 15:55:43 -0300

    Home created

---
+++ src/test/java/br/com/ProjectLicitation/ProjectLicitationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ProjectLicitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLicitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
